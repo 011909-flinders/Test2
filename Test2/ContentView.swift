d3839734eeb8948844e3489662bb17ef7d771170
Ed <012028@mfac.edu.au>
2025-11-18 13:13:52 +1000

    Epic changes again

--- Test2/ContentView.swift
+++ Test2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! Sigma Cool Epic Changes")
+            Text("Hello, world! Sigma Cool Epic Changes, Another one")
         }
         .padding()
     }
