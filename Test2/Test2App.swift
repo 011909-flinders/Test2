ceeb1e1988375baba9c99562725fd031501fc6fa
LukeBarbeau <011909@mfac.edu.au>
2025-11-18 11:54:43 +1000

    Initial Commit

---
+++ Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Luke Barbeau on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
